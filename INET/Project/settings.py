50eaacc74cb4ca0225884fd9b27c9990e23e0668
Bruno Ezeqiel Sánchez <3zequielbsanchez@gmail.com>
2025-06-15 21:07:58 -0300

    Add files via upload

--- INET/Project/settings.py
+++ INET/Project/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+MEDIA_URL = '/Media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
+
+LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
