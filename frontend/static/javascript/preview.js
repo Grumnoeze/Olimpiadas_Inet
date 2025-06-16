0f1591c7688c201c72c7e8deb9032ab869d34d28
mts-mlna <matiasezequielmolina2006@gmail.com>
2025-06-16 01:19:46 -0300

    Añadido las páginas exclusivas para el administrador/superusuario

---
+++ frontend/static/javascript/preview.js
@@ -0,0 +1,9 @@
+ document.getElementById('upload').addEventListener('change', function(event) {
+    var reader = new FileReader();
+    reader.onload = function() {
+        var output = document.getElementById('preview');
+        output.src = reader.result;
+        output.style.display = 'block';
+    }
+    reader.readAsDataURL(event.target.files[0]);
+});
